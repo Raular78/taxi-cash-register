c053aadfb6399f2c29a73e0fa72faf1fb99d2726
Raular_78 <shukothay@gmail.com>
2025-05-18 15:05:37 +0200

    Configurar alias @/ para producción

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': '.',
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
